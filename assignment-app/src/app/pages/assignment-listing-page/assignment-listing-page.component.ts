1668bd7c2494de69da034734da7d60b5f12d7fc9
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2024-10-23 19:27:34 +0200

    add assignment button

--- assignment-app/src/app/pages/assignment-listing-page/assignment-listing-page.component.ts
+++ assignment-app/src/app/pages/assignment-listing-page/assignment-listing-page.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {AssignmentListComponent} from "../../components/assignment-list/assignment-list.component";
+import {MatFabButton} from "@angular/material/button";
+import {AssignmentFormComponent} from "../../components/assignment-form/assignment-form.component";
+import {MatBottomSheet} from "@angular/material/bottom-sheet";
+import {AuthService} from "../../../shared/services/auth.service";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-assignment-listing-page',
   standalone: true,
   imports: [
-    AssignmentListComponent
+    AssignmentListComponent,
+    MatFabButton,
+    MatIcon
   ],
   templateUrl: './assignment-listing-page.component.html',
 })
 export class AssignmentListingPageComponent {
 
+  private _bottomSheet = inject(MatBottomSheet);
+
+  constructor(public authService: AuthService) {
+  }
+
+  openNewAssignmentFormSheet() {
+    this._bottomSheet.open(AssignmentFormComponent);
+  }
 }
