1ecea836657ae177ba2a080ea33d5bd86024c687
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2024-11-22 08:49:20 +0100

    fix logout

--- assignment-app/src/shared/services/auth.service.ts
+++ assignment-app/src/shared/services/auth.service.ts
@@ -6,8 +6,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private _isAuthenticated: boolean = false;
 
-  constructor() { }
-
+constructor() { }
   async login(login: string, password: string): Promise<string> {
     this._isAuthenticated = true;
 
@@ -18,10 +17,10 @@ export class AuthService {
     });
   }
 
-  async logout() : Promise<void> {
+  async logout() : Promise<boolean> {
     return new Promise((resolve, reject) => {
       this._isAuthenticated = false;
-      resolve();
+      resolve(false);
     });
   }
 
